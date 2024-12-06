08f57d031eb408166bd12b9f5d282e149447dd0e
Vladimir Ermakov <vooon341@gmail.com>
2024-12-06 09:50:52 +0100

    ci: add script to prepare plugins

---
+++ ci/release-oci-images.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+
+copybin() {
+	local gorel=$1
+	local plugarch=$2
+	mkdir -p "dist/$plugarch/plugin"
+	cp -p "dist/$gorel/bin"/* "dist/$plugarch/plugin/"
+}
+
+# move binaries to places expected by fleetine-artifact
+copybin fleeting-plugin-openstack_linux_386_sse2 linux/386
+copybin fleeting-plugin-openstack_linux_amd64_v1 linux/amd64
+copybin fleeting-plugin-openstack_linux_arm64_v8.0 linux/arm64
+copybin fleeting-plugin-openstack_linux_arm_7 linux/armv7
+
+go install gitlab.com/gitlab-org/fleeting/fleeting-artifact/...@latest
